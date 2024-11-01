05b3912dd0aff2d08040b91d7888965d3cf774e3
ipz243_kov <ipz243_kov@student.ztu.edu.ua>
2024-11-01 19:06:04 +0200

    All Tasks

---
+++ LinearExpressions1/Program.cs
@@ -0,0 +1,22 @@
+﻿double a, b, c, d;
+
+Console.Write("Enter a: ");
+a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter b: ");
+b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter c: ");
+c = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter d: ");
+d = Convert.ToDouble(Console.ReadLine());
+
+double x = (a + 2 * b - c + d) / c / d + (a + b) / (c - d) - a * a / b / b;
+Console.WriteLine("x = {0}", x);
+
+double y = 5 * (a + b) * (c - d) / (1f / 2) * c + d * d * (a * a - b * b) / (b - a);
+Console.WriteLine("y = {0}", y);
+
+double z = (Math.Pow(x * x - 2 * x, 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b) / (5 * a + 3 * b);
+Console.WriteLine("z = {0}", z);
+
+double r = (1f / 2 * a + 3f / 4 * b - 7f / 5) / (3 * c + 1) + 1 / (a - c);
+Console.WriteLine("r = {0}", r);
---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,19 @@
+﻿double a, b, m, n, x;
+
+Console.Write("Enter m: ");
+m = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter n: ");
+n = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter a: ");
+a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter b: ");
+b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter x: ");
+x = Convert.ToDouble(Console.ReadLine());
+
+double z1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / (Math.Sqrt(m * m * m * n) + n * m + m * m - m);
+Console.WriteLine("z1 = {0}", z1);
+double z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+Console.WriteLine("z2 = {0}", z2);
+double y = 2.4 * Math.Abs((x * x + b) / a) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + 0.01 * (x - b);
+Console.WriteLine("y = {0}", y);
